029997729b629bba45fcfa631dd8d94eefc41ad1
Max Freed <drmaxfreed@gmail.com>
2017-08-14 20:56:22 -0400

    Exercise 3 solution.

--- main.py
+++ main.py
@@ -3,5 +3,8 @@
 def celsius_to_farenheit(c):
     return c * 9/5 + 32
 
-temp = int(input("Enter temp in C: "))
-print(celsius_to_farenheit(temp))
+temp = float(input("Enter temp in C: "))
+if temp < -273.15:
+    print("This is an impossible temperature, less than absolute zero.")
+else:
+    print(celsius_to_farenheit(temp))
